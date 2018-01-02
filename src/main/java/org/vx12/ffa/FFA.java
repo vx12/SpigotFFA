72eeb89adc9dc7ee4e8dcfbc18ba7d43a7c5456e
vx12 <carrypotterbusiness@gmail.com>
2018-01-02 19:12:14 +0100

    edit:    FFA.java added:    plugin.yml

--- src/main/java/org/vx12/ffa/FFA.java
+++ src/main/java/org/vx12/ffa/FFA.java
@@ -3,4 +3,18 @@ package org.vx12.ffa;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FFA extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+        getLogger().info("FFA system enabled");
+    }
+
+
+    @Override
+    public void onDisable() {
+
+        getLogger().info("FFA system disabled");
+
+
+    }
 }
